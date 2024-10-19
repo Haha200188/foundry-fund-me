8e521566ebea3af7828caac502c98fee2838e0e4
Haha200188 <97596109+Haha200188@users.noreply.github.com>
2024-10-19 17:45:22 +0800

    main contract and lib

---
+++ src/FundMe.sol
@@ -0,0 +1,101 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+error NotEnoughETH(uint256 sent, uint256 required);
+error CallFailed();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    address public immutable I_OWNER;
+    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
+
+    constructor() {
+        I_OWNER = msg.sender;
+    }
+    function fund() public payable {
+        // require(
+        //     msg.value.getConversionRate() >= MINIMUM_USD,
+        //     "You need to spend more ETH!"
+        // );
+        if (msg.value.getConversionRate() < MINIMUM_USD) {
+            revert NotEnoughETH({
+                sent: msg.value.getConversionRate(),
+                required: MINIMUM_USD
+            });
+        }
+
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        // ETH/USD price feed address of Sepolia Network.
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != I_OWNER) revert NotOwner();
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        if (!callSuccess) {
+            revert CallFailed();
+        }
+    }
+
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+}
